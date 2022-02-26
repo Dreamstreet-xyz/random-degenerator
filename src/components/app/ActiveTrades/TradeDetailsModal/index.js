a699154274f08385e4d440eebf556468285c29a5
dreamersnat <dreamersnat@protonmail.com>
2022-02-25 21:35:12 -0800

    Basic styling on TradeDetailsModal

--- src/components/app/ActiveTrades/TradeDetailsModal/index.js
+++ src/components/app/ActiveTrades/TradeDetailsModal/index.js
@@ -1,7 +1,20 @@
 import { formatEther, formatUnits } from '@ethersproject/units';
 import { Button, Modal } from 'components/common';
 import useLivePnl from 'shared/hooks/useLivePnl';
-import { ModalBody } from './styles';
+import {
+    ModalBody,
+    Row,
+    Label,
+    Pair,
+    Position,
+    Collateral,
+    Size,
+    Open,
+    Tp,
+    Sl,
+    Pnl,
+    CloseButton,
+} from './styles';
 
 export default function TradeDetailsModal({ isVisible, close, trade, tradeInfo }) {
     const { pairString, leverage, buy, positionSizeDai, sl, tp, openPrice } = trade;
@@ -14,32 +27,49 @@ export default function TradeDetailsModal({ isVisible, close, trade, tradeInfo }
             close={close}
             title=""
             titleStyle={{ color: '#6b69ee' }}
-            containerStyle={{ maxWidth: 550, width: '100%' }}
+            containerStyle={{ maxWidth: 350, width: '100%' }}
         >
             <ModalBody>
-                <p>{pairString}</p>
-                <p>
+                <Pair>{pairString}</Pair>
+                <Position buy={buy}>
                     {leverage}x {buy ? 'LONG' : 'SHORT'}
-                </p>
-                <p>Collateral: {Number(formatEther(positionSizeDai)).toFixed(1)} DAI</p>
-                <p>Size: {Number(formatEther(openInterestDai)).toFixed(0)} DAI</p>
-                <p>Open Price: {formatUnits(openPrice, 10)}</p>
-                <p>Take Profit: {formatUnits(tp, 10)}</p>
-                <p>Stop Loss: {formatUnits(sl, 10)}</p>
-                <p>
-                    Net PnL:{' '}
-                    {pnl.pnlInclFee.toLocaleString(undefined, {
-                        maximumFractionDigits: 2,
-                        minimumFractionDigits: 2,
-                    })}{' '}
-                    DAI ( {pnl.pnlInclFee > 0 ? '+' : ''}
-                    {(pnl.percentProfitInclFee * 100).toLocaleString(undefined, {
-                        maximumFractionDigits: 2,
-                        minimumFractionDigits: 2,
-                    })}
-                    %)
-                </p>
-                <Button title="Close Trade" />
+                </Position>
+                <Row>
+                    <Label>Collateral</Label>
+                    <Collateral>{Number(formatEther(positionSizeDai)).toFixed(1)} DAI</Collateral>
+                </Row>
+                <Row>
+                    <Label>Size</Label>
+                    <Size>{Number(formatEther(openInterestDai)).toFixed(0)} DAI</Size>
+                </Row>
+                <Row>
+                    <Label>Open Price</Label>
+                    <Open>{Number(formatUnits(openPrice, 10)).toFixed(6)}</Open>
+                </Row>
+                <Row>
+                    <Label>Take Profit</Label>
+                    <Tp>{tp > 0 ? Number(formatUnits(tp, 10)).toFixed(5) : 'N/A'}</Tp>
+                </Row>
+                <Row>
+                    <Label>Stop Loss</Label>
+                    <Sl>{sl > 0 ? Number(formatUnits(sl, 10)).toFixed(5) : 'N/A'}</Sl>
+                </Row>
+                <Row>
+                    <Label>Net PnL</Label>
+                    <Pnl pnl={pnl}>
+                        {pnl.pnlInclFee.toLocaleString(undefined, {
+                            maximumFractionDigits: 2,
+                            minimumFractionDigits: 2,
+                        })}{' '}
+                        DAI ( {pnl.pnlInclFee > 0 ? '+' : ''}
+                        {(pnl.percentProfitInclFee * 100).toLocaleString(undefined, {
+                            maximumFractionDigits: 2,
+                            minimumFractionDigits: 2,
+                        })}
+                        %)
+                    </Pnl>
+                </Row>
+                <CloseButton title="Close Trade" />
             </ModalBody>
         </Modal>
     );
--- src/components/app/ActiveTrades/TradeDetailsModal/styles.js
+++ src/components/app/ActiveTrades/TradeDetailsModal/styles.js
@@ -1,5 +1,70 @@
+import { Button } from 'components/common';
 import styled, { css } from 'styled-components';
 
 export const ModalBody = styled.div`
     color: white;
+`;
+
+export const Row = styled.div`
+    display: flex;
+    align-items: flex-end;
+    font-size: 20px;
+    margin-bottom: 24px;
+`;
+
+export const Label = styled.span`
+    font-size: 16px;
+    color: #a694ec;
+`;
+
+const fieldStyle = css`
+    font-size: 20px;
+    font-weight: bold;
+    margin-left: auto;
+`;
+
+export const Pair = styled.p`
+    font-size: 36px;
+    font-weight: bold;
+    color: #57e08b;
+    text-align: center;
+`;
+
+export const Position = styled.p`
+    font-size: 24px;
+    font-weight: bold;
+    color: ${({ buy }) => (buy ? '#57e08b' : '#db5c91')};
+    text-align: center;
+    margin-bottom: 32px;
+`;
+
+export const Collateral = styled.p`
+    ${fieldStyle}
+`;
+
+export const Size = styled.p`
+    ${fieldStyle}
+`;
+
+export const Open = styled.p`
+    ${fieldStyle}
+`;
+
+export const Tp = styled.p`
+    ${fieldStyle}
+`;
+
+export const Sl = styled.p`
+    ${fieldStyle}
+`;
+
+export const Pnl = styled.p`
+    ${fieldStyle}
+    color: ${({ pnl }) => (pnl.pnlInclFee > 0 ? '#57e08b' : '#db5c91')};
+`;
+
+export const CloseButton = styled(Button)`
+    width: 100%;
+    padding: 16px;
+    font-size: 20px;
 `;
