065c2bb17f91ae07a8e8f7b55c055c2329aeb2fa
thebuidler <thebuidler@pm.me>
2022-02-28 17:52:41 -0600

    Removed log

--- src/components/app/ActiveTrades/TradeDetailsModal/index.tsx
+++ src/components/app/ActiveTrades/TradeDetailsModal/index.tsx
@@ -31,7 +31,6 @@ export default function TradeDetailsModal({
     const pnl = useLivePnl({ trade, tradeInfo });
     const price = useLivePrice({ trade, tradeInfo });
 
-    console.log(trade);
     const handleClose = e => {
         e.stopPropagation();
         onCloseTrade?.();
